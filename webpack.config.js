6fd87e150ea9f1debd72cf4948d61466ab9b3fc8
ArturIlchenko <artur.v.ilchenko@yandex.ru>
2022-01-25 16:27:19 +0500

    add dev-server to HW1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode: 'development',
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 	entry: {
 		main: path.resolve(__dirname, './src/index.js'),
 	},
